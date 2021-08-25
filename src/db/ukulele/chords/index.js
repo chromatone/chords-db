7c2c988c6aa78379518e240452945b231c11e521
davay42 <fond@fondfrukt.ru>
2021-08-26 00:03:04 +0300

    renamed all folders

--- src/db/ukulele/chords/index.js
+++ src/db/ukulele/chords/index.js
@@ -1,14 +1,27 @@
-import A from './A';
-import Ab from './Ab';
-import B from './B';
-import Bb from './Bb';
-import C from './C';
-import D from './D';
-import Db from './Db';
-import E from './E';
-import Eb from './Eb';
-import F from './F';
-import G from './G';
-import Gb from './Gb';
+import C from './C/index'
+import Csharp from './C#/index'
+import D from './D/index'
+import Dsharp from './D#/index'
+import E from './E/index'
+import F from './F/index'
+import Fsharp from './F#/index'
+import G from './G/index'
+import A from './A/index'
+import Gsharp from './G#/index'
+import B from './B/index'
+import Asharp from './A#/index'
 
-export default { A, Ab, B, Bb, C, D, Db, E, Eb, F, G, Gb };
+export default {
+  C,
+  'C#': Csharp,
+  D,
+  'D#': Dsharp,
+  E,
+  F,
+  'F#': Fsharp,
+  G,
+  'G#': Gsharp,
+  A,
+  'A#': Asharp,
+  B,
+}
